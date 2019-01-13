3bcd17187270057d04dfaf1f696435418f08ecf5
makehoney <makehoney3@gmail.com>
2019-01-13 20:16:20 +0900

    Add on click listeners

--- app/src/main/java/com/brian/swoosh/Controller/SkillActivity.kt
+++ app/src/main/java/com/brian/swoosh/Controller/SkillActivity.kt
@@ -1,16 +1,43 @@
 package com.brian.swoosh.Controller
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.brian.swoosh.Utils.EXTRA_LEAGUE
+import com.brian.swoosh.Utils.EXTRA_SKILL
 import com.brian.swoosh.R
+import kotlinx.android.synthetic.main.activity_skill.*
 
 class SkillActivity : AppCompatActivity() {
     var league = ""
+    var skill = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill)
         league = intent.getStringExtra(EXTRA_LEAGUE)
         println(league)
+    }
+
+    fun onSkillFinishClicked (view: View) {
+        if (skill != "") {
+            val finishActivity = Intent(this, FinishActivity::class.java)
+            finishActivity.putExtra(EXTRA_SKILL, skill)
+            finishActivity.putExtra(EXTRA_LEAGUE, league)
+            startActivity(finishActivity)
+        } else {
+            Toast.makeText(this, "Please select a skill level", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun onBeginnerClicked (view: View) {
+        ballerSkillBtn.isChecked = false
+        skill = "beginner"
+    }
+
+    fun onBallerClicked (view: View) {
+        beginnerSkillBtn.isChecked = false
+        skill = "baller"
     }
 }
